968a09371ec52632d2513d73a0ce15be4e753662
johannkouame <kouameremy4@gmail.com>
2025-03-10 22:46:55 +0100

    Update: Add AwsRepository to centralized methods

--- src/command/manage_buckets.py
+++ src/command/manage_buckets.py
@@ -1,14 +1,19 @@
 import logging
-
 from datasets import load_dataset
-from src.client.aws_s3_client import S3
+from src.client.aws_s3 import S3
 from tqdm import tqdm
 from src.logger.logger import Logger
 import click
+from src.repository.aws_s3_repository import AwsS3Repository
 
 LOGGING_VARIABLE = "[Data manager]"
 
 Logger()
+@click.group()
+def cli():
+    """Command-line tool for data management."""
+    pass
+
 
 @click.command()
 @click.option("--empty", default=False, type=click.BOOL, help="Put True if you want to empty bucket au lieu de les remplir")
@@ -16,68 +21,29 @@ Logger()
 @click.option("--dataset_name", default="wikitext-2-raw-v1", help="Specific dataset in the repo")
 @click.option("--bucket_name", default="raw", help="Bucket name(s) where to store data. If many buckets, put them into bracket like a list.")
 def manage_data(empty, repo: str, dataset_name: str, bucket_name: str):
-    s3_client = S3()
+    aws_s3 = AwsS3Repository(S3())
     if empty:
-        
-        if type(bucket_name) is str:
-            logging.info(msg=f"{LOGGING_VARIABLE} Empty bucket")
-            
-            # Si aucun fichier n'est précisé, on supprime tout le contenu du bucket
-            response = s3_client.list_objects_v2(bucket_name)
-            if 'Contents' in response:
-                files_to_delete = [obj['Key'] for obj in response['Contents']]
-                delete_objects = {'Objects': [{'Key': file} for file in files_to_delete]}
-                s3_client.delete_objects(Bucket=bucket_name, Delete=delete_objects)
-                print(f"{len(files_to_delete)} fichier(s) supprimé(s) du bucket {bucket_name}.")
-            else:
-                logging.info(msg=f"{LOGGING_VARIABLE} Bucket already empty.")
-        elif type(bucket_name) is list:
-            for bucket in bucket_name:
-                logging.info(msg=f"{LOGGING_VARIABLE} Empty bucket")
-                # Si aucun fichier n'est précisé, on supprime tout le contenu du bucket
-                response = s3_client.list_objects_v2(bucket_name)
-                if 'Contents' in response:
-                    files_to_delete = [obj['Key'] for obj in response['Contents']]
-                    delete_objects = {'Objects': [{'Key': file} for file in files_to_delete]}
-                    s3_client.delete_objects(Bucket=bucket_name, Delete=delete_objects)
-                    print(f"{len(files_to_delete)} fichier(s) supprimé(s) du bucket {bucket_name}.")
-                else:
-                    logging.info(msg=f"{LOGGING_VARIABLE} Bucket already empty.")
+        pass
     else:
-                # Charger les données WikiText
+        # Upload files
         logging.info(msg=f"{LOGGING_VARIABLE} Downloading data from HuggingFace...")
         data = load_dataset(repo, dataset_name)
+        split = ["train", "test", "validation"]
         logging.info(msg=f"{LOGGING_VARIABLE} Data successfully downloaded.")
 
+        # Browse each split and transfer data
         logging.info(msg=f"{LOGGING_VARIABLE} Transferring data to S3 bucket...")
-        # Parcourir chaque split et téléverser les données
-        for split in ["train", "test", "validation"]:
-            split_data = data[split]
-            logging.info(msg=f"{LOGGING_VARIABLE} Transferring {split} data...")
-
-            # Utiliser tqdm pour afficher une barre de progression
-            for idx, unit in tqdm(enumerate(split_data), desc=f"Uploading {split}", total=split_data.num_rows):
-                # Texte à téléverser
-                text_data = unit['text']
-
-                # Clé pour organiser les fichiers par split
-                key = f"{split}/{split}_{idx}.txt"
-
-                # Téléverser les données directement depuis la mémoire
-                s3_client.put_object(bucket_name, key, text_data)
-
-        logging.info(msg=f"{LOGGING_VARIABLE} Data successfully uploaded to the S3 bucket named 'raw'.")
-        
-        # Lister les dossiers
-        logging.info(msg=f"Listing folders in bucket: {bucket_name}")
-        response = s3_client.list_objects_v2(Bucket=bucket_name, Delimiter='/')
-
-        if 'CommonPrefixes' in response:
-            for prefix in response['CommonPrefixes']:
-                logging.info(msg=f"{LOGGING_VARIABLE} {prefix['Prefix']}")  # Affiche chaque dossier
+        response = aws_s3.upload_files_into_multiple_folders_test(Bucket=bucket_name, data=data, folders=split)
+        if response:
+            logging.info(msg=f"{LOGGING_VARIABLE} Data successfully uploaded to the S3 bucket named {bucket_name}")
+            
+            logging.info(msg=f"{LOGGING_VARIABLE} Listing folders in bucket: {bucket_name}")
+            response = aws_s3.list_files(Bucket=bucket_name, Delimiter="/")
+            folders = aws_s3.list_folders(Bucket=bucket_name, folders=response['CommonPrefixes'])
+            for folder, count in folders.items():
+                logging.info(f"{LOGGING_VARIABLE} Folder : {folder} - File count: {count}")
         else:
-            logging.info(msg=f"{LOGGING_VARIABLE} No folders found in the bucket.")
-
+            logging.info(msg=f"{LOGGING_VARIABLE} Error durinng uploading files to S3 bucket named {bucket_name}")
+        # Listing folders
 if __name__ == "__main__":
-    manage_data()
-    
+    cli()
---
+++ src/repository/aws_s3_repository.py
@@ -0,0 +1,98 @@
+from src.client.aws_s3 import S3
+import logging
+from tqdm import tqdm
+import traceback
+
+LOGGING_VARIABLE = "[AWS S3]"
+
+class AwsS3Repository:
+    def __init__(self, s3_client:S3):
+        self.s3_client = s3_client
+
+    def list_files(self, Bucket:str, Prefix:str="", Delimiter:str="") -> list:
+        response = self.s3_client.list_objects_v2(Bucket=Bucket, Prefix=Prefix, Delimiter=Delimiter)
+
+        return response
+
+    def list_folders(self, Bucket:str, folders: dict) -> dict:
+        response = {}
+        for folder in folders:
+            folder_name = folder['Prefix']
+
+            folder_response = self.list_files(Bucket=Bucket, Prefix=folder_name)
+            file_count = len(folder_response.get('Contents', []))
+            response[folder_name] = file_count
+        return response
+
+    def process_data(self, Bucket, Prefix, process_func) -> dict:
+        try:
+            logging.info(msg=f"{LOGGING_VARIABLE} Getting list of {Bucket}'s files...")
+            files = self.list_files(Bucket, Prefix)
+            processed_data = {}
+
+            for file_key in files:
+                logging.info(msg=f"{LOGGING_VARIABLE}📥 Download {file_key} from {Bucket}...")
+                response = self.s3_client.get_object(Bucket=Bucket, Key=file_key)
+
+                logging.info(msg=f"{LOGGING_VARIABLE}⚙️ Prétraitement du fichier {file_key}...")
+                raw_text = response['Body'].read().decode('utf-8')
+                clean_text = process_func(raw_text)
+                processed_data[file_key] = clean_text
+
+            return  processed_data
+        except Exception as exception:
+            logging.critical(f"{LOGGING_VARIABLE} Error while processing data {Bucket} ...", extra={"error":exception})
+            return None
+
+    def upload_files_into_one_folder(self, Bucket:str, Key:str, Body:str) -> bool:
+        try:
+            logging.info(f"{LOGGING_VARIABLE} 🚀 Uploading into {Bucket} ...")
+            self.s3_client.put_object(Bucket=Bucket, Key=Key, Body=Body.encode('utf-8'))
+
+            return True
+        except Exception as exception:
+            logging.critical(f"{LOGGING_VARIABLE} Error while uploading data into {Bucket} ...", extra={"error":exception})
+            return False
+
+    def upload_files_into_multiple_folders_test(self, folders:list,
+                                                data,
+                                                Bucket:str) -> bool:
+        try:
+            logging.info(f"{LOGGING_VARIABLE} 🚀 Uploading into {Bucket} ...", extra={"folders": folders})
+            for split in folders:
+                split_data = data[split]
+                logging.info(msg=f"{LOGGING_VARIABLE} Transferring {split} data...", extra={"split_data": split_data})
+                for idx, unit in tqdm(enumerate(list(split_data['text'][:100])), desc=f"Uploading {split}", total=100):
+                    # Text to upload
+                    text_data = unit
+                    # Path
+                    key = f"{split}/{split}_{idx}.txt"
+                    # Transfer into bucket
+                    self.s3_client.put_object(Bucket=Bucket, Key=key, Body=text_data)
+
+            return True
+        except Exception as exception:
+            logging.critical(f"{LOGGING_VARIABLE} Error while uploading data into {Bucket} ...", extra={"error":exception,
+                                                                                                        "traceback":traceback.format_exc()})
+
+    def upload_files_into_multiple_folders(self, folders:list,
+                                                data,
+                                                Bucket:str) -> bool:
+        try:
+            logging.info(f"{LOGGING_VARIABLE} Uploading into {Bucket} ...")
+            for split in folders:
+                split_data = data[split]
+                logging.info(msg=f"{LOGGING_VARIABLE} Transferring {split} data...")
+
+                for idx, unit in tqdm(enumerate(split_data), desc=f"Uploading {split}", total=len(split_data.num_rows)):
+                    # Text to upload
+                    text_data = unit['text']
+                    # Path
+                    key = f"{split}/{split}_{idx}.txt"
+                    # Transfer into bucket
+                    self.s3_client.put_object(Bucket=Bucket, Key=key, Body=text_data)
+
+            return True
+        except Exception as exception:
+            logging.critical(f"{LOGGING_VARIABLE} Error while uploading data into {Bucket} ...", extra={"error":exception})
+            return False
